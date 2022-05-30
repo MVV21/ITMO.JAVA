63073ed3db916da5b9fd17d85731d1e2dee178a1
MVV21 <medvedev290691@yandex.ru>
2022-05-30 17:22:25 +0300

    Задание №7

--- src/org/lession7/Plane.java
+++ src/org/lession7/Plane.java
@@ -16,15 +16,5 @@ public class Plane {
             this.wingWeight = wingWeight;
         }
 
-        public static void main(String[] args) {
-            Plane.Wing object1 = new Plane.Wing();
-            object1.setWingWeight(500);
-            object1.getWingWeight();
-
-            Plane.Wing object2 = new Plane.Wing();
-            object2.setWingWeight(800);
-            object2.getWingWeight();
-        }
-
     }
 }
