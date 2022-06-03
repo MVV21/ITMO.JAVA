553975b5d275bb9f83358de721cfb6b809109e67
MVV21 <medvedev290691@yandex.ru>
2022-06-04 02:24:49 +0300

    Задание №10

--- src/org/lession10/Main.java
+++ src/org/lession10/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         File file1 = new File("src/Text.txt");
         File file2 = new File("src/newText.txt");
+
 //        Задание №1
 //        System.out.println(firstMethod(file1));
 
